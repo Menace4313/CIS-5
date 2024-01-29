e34a05d404c4b536c73686cb0f72399ac6d026e0
Menace4313 <158023072+Menace4313@users.noreply.github.com>
2024-01-28 22:48:58 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+/* 
+ * File:   main.cpp
+ * Author: GABRIAL
+ *
+ * Created on January 5, 2024, 12:11 PM
+ * Purpose: Sum of two numbers
+ */
+
+//system libraries
+
+#include <iostream>  //Imput-Output Library
+
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math, Physics, Chemistry, Conversions,
+
+
+//Function Prototypes 
+
+
+//Program Execution Begins Here
+
+int main(int argc, char** argv) {
+    //set a random seed
+  
+    
+    //Declare all variables
+   int op1, // variable 1
+       op2, // variable 2
+       rslt;
+            
+    // Initialize all variables
+       op1= 100;
+       op2= 50;
+    
+    //Process or map solutions
+       
+    rslt=op1+op2; // addition  
+
+    //display the out put
+    cout<<op1
+        <<"+"
+        <<op2
+        <<"="
+        <<rslt
+        <<endl;
+    //Exit the program
+    return 0;
+}
